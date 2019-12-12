dc2dd22486e6b45e4e3a5533587eea7605c2612e
penglin <plin@qq.com>
2019-12-12 15:24:25 +0800

    2

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,32 +1,40 @@
 <template>
   <div class="home">
     <h1>Webpack4.30.0 For VUE templete</h1>
-    <router-link :to="{ name:'info' }">详情</router-link>
-    <router-link :to="{ name:'h5' }">H5</router-link>
+    <router-link :to="{ name:'info' }">
+      详情
+    </router-link>
+    <router-link :to="{ name:'h5' }">
+      H5
+    </router-link>
     <div>
-      <el-button type="primary" @click="show = true">弹框</el-button>
+      <el-button type="primary" @click="show = true">
+        弹框
+      </el-button>
     </div>
-    
+
     <div>name: {{ name }}</div>
     <div>lovingVue: {{ lovingVue }}</div>
     <div>子组件中的年龄是：{{ childrens }}</div>
     <div>输入框：{{ inputsValue }}</div>
     <div>自定义事件：{{ onmsg }}</div>
-    <list :name.sync="name" @callback="callback"></list>
+    <list :name.sync="name" @callback="callback" />
 
-    <base-checkbox ref="basecheck" v-model="lovingVue"></base-checkbox>
+    <base-checkbox ref="basecheck" v-model="lovingVue" />
 
     <inputs
+      v-model="inputsValue"
       data-a="penglin"
       placeholder="你好呀"
       @input="inputsfunc"
       @focus="focusfunc"
-      v-model="inputsValue"
-    ></inputs>
+    />
 
-    <dia :show.sync="show"></dia>
+    <dia :show.sync="show" />
   </div>
 </template>
 
-<script src="./home.js"></script>
-<style lang="less" scoped src="./home.less"></style>
+<script src="./home.js">
+</script>
+<style lang="less" scoped src="./home.less">
+</style>
